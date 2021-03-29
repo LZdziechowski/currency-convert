8928dab68541f7d67e7dd4cf276cdbab1e1b6f42
LZdziechowski <l.zdziechowski.contact@gmail.com>
2021-03-29 20:16:59 +0200

    add logger to ConversionService class

--- src/main/java/com/currencyconvert/service/ConvertService.java
+++ src/main/java/com/currencyconvert/service/ConvertService.java
@@ -4,6 +4,8 @@ import com.currencyconvert.domain.Convert;
 import com.currencyconvert.domain.CurrencyRate;
 import com.currencyconvert.domain.RatesTable;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -14,19 +16,26 @@ import java.util.Collection;
 @RequiredArgsConstructor
 public class ConvertService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConvertService.class);
+
     private final NBPService nbpService;
 
     public String convertCurrency(Convert convert) {
+        LOGGER.info("Getting rate for: " + convert.getFromCurrency());
         BigDecimal currencyRateFrom = getCurrencyRate(convert.getFromCurrency());
+        LOGGER.info("Getting rate for: " + convert.getToCurrency());
         BigDecimal currencyRateTo = getCurrencyRate(convert.getToCurrency());
+        LOGGER.info("Starting conversion for amount: " + convert.getAmount().toString());
         BigDecimal resultAmount = convert.getAmount()
                 .multiply(currencyRateFrom)
                 .divide(currencyRateTo, 2, RoundingMode.HALF_EVEN);
+        LOGGER.info("Conversion finished with result: " + resultAmount.toString());
         return resultAmount.toString();
     }
 
     private BigDecimal getCurrencyRate(String currency) {
         if (currency.equalsIgnoreCase("złoty (Polska)")) {
+            LOGGER.info("Rate is: 1.0");
             return BigDecimal.ONE;
         }
         CurrencyRate currencyRate = nbpService.getAvailableCurrencyRates().stream()
@@ -34,6 +43,13 @@ public class ConvertService {
                 .flatMap(Collection::stream)
                 .filter(cr -> cr.getCurrency().equalsIgnoreCase(currency))
                 .findFirst().orElse(null);
-        return currencyRate.getMid();
+        if (currencyRate == null) {
+            LOGGER.error("Wrong currency name");
+            LOGGER.info("Converting has been aborted");
+            throw new IllegalStateException("Wrong currency name: " + currency);
+        } else {
+            LOGGER.info("Rate is: " + currencyRate.getMid().toString());
+            return currencyRate.getMid();
+        }
     }
 }
